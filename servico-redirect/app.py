70f9d86257f382c9a967f5aa69988105778182ab
Renato de souza lucas <42358704+renatodesouza@users.noreply.github.com>
2025-11-09 18:11:51 -0300

    feat(redirect): adiciona endpoints de redirecionamento e integração com serviço de links

---
+++ servico-redirect/app.py
@@ -0,0 +1,38 @@
+from flask import flask, redirect, abort
+import requests
+import os
+
+
+app = Flask(__name__)
+
+# O nome 'servico-links' funciona por causa do Docker Compose
+LINKS_API_URL = "http://servico-links:8000/api/internal-lookup/"
+
+@app.route('/<string:short_code>')
+def redirect_to_url(short_code):
+    try:
+        # Pergunta ao Serviço de Links qual e a URL longa
+        # Esta é a comunicação microsserviço-para-microsserviço! 
+        response = requests.get(f'{LINKS_API_URL}{short_code}')
+
+        if response.status_code == 200:
+            data = response.json()
+            # Redireciona o usuário
+            # (Aqui você redirecionaria o evento de analytics)
+            return redirect(data['original_url'], code=302)
+        else:
+            abort(404)
+
+    except requests.exceptions.ConnectionError:
+        # Serviço de links está fora do ar
+        abort(503) # Service Unavailable
+
+@app.route('/')
+def health_check():
+    # Rota raiz (/) apenas para mostrar que o serviço está no ar
+    # O Nginx vai capturar isso
+    return "Serviço de Redirect está no ar. Use /<codigo> para redirecionar."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+        
