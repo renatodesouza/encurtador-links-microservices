44ed382105fd3627aebbd854ea1e9e006e3fc5bd
Renato de souza lucas <42358704+renatodesouza@users.noreply.github.com>
2025-11-15 15:59:29 -0300

    feat(auth): cria o serializer para registrar um usuário

---
+++ servico-auth/usuarios/serializers.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserRegisterSerializer(serializers.modelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+        extra_kwargs = {'password':{'write_only':True}}
+
+    def create(self, validated_data):
+        # Cria o usuário com o password "hash" (criptografado)
+        user = user.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+
+        return user
